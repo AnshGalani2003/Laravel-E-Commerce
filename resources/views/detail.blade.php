5e122935846b5cd5c74429bf7d856c3a22b9b0bf
AnshGalani2003 <anshgalani@yahoo.com>
2024-07-03 22:31:55 +0530

    Add Product in cart table

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -15,7 +15,11 @@
                     <h4>Category: {{ $product['category'] }}</h4>
                     <p>Description: {{ $product['description'] }}</p>
                     <br>
-                    <button class="btn btn-success">Add to Cart</button>
+                    <form action="/add_to_cart" method="POST">
+                        @csrf
+                        <input type="hidden" name="product_id" value="{{ $product['id'] }}">
+                        <button class="btn btn-success">Add to Cart</button>
+                    </form>
                     <br><br>
                     <button class="btn btn-primary">Buy Now</button>
                 </div>
